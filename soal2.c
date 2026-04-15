2e2cdcd8a87f6a436631bf53c5f9df3dfdb6b9c8
zulfanhanani <hanani.zulfan@gmail.com>
2026-04-15 08:38:09 +0700

    Implement transformation function for number steps
    Refactor main function and add transformation logic

--- soal2.c
+++ soal2.c
@@ -1,5 +1,40 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 1
+ *   Hari dan Tanggal    : 15 April 2026
+ *   Nama (NIM)          : Zulfan Hanani (13224105)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : KODE BATU STABIL
+ *                         Membuat program untuk menentukan jumlah langkah pada transformasi sebuah bilangan hingga ke angka 1
+ * 
+ */
 
-int main(void) {
+ #include <stdio.h>
+ #include <string.h>
+
+ void transformasi(int n, int *langkah){
+    if (n % 2 == 0){
+        (*langkah)++;
+        transformasi(n/2, langkah);
+    }else if (n % 2 == 1 & n > 1){
+        (*langkah)++;
+        transformasi(3*n+1, langkah);
+    }else{
+        return;
+    }
+ }
+  
+ int main() {
+    int T;
+    scanf("%d", &T);
+    int n[T], langkah[T];
+    for (int i = 0; i < T; i++){
+        scanf("%d", &n[i]);
+        langkah[i] = 0;
+    }
+    for (int i = 0; i < T; i++){
+        transformasi(n[i], &langkah[i]);
+        printf("\nLANGKAH %d\n", langkah[i]);
+    }
     return 0;
-}
+ }
+ 
